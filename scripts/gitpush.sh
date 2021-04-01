19dc6d6e6717b110603e8ca8ad7766fc1217544e
root <demo@hashicorp.com>
2021-04-01 11:47:08 -0700

    git push script

--- scripts/gitpush.sh
+++ scripts/gitpush.sh
@@ -1,9 +1,16 @@
+#!/bin/bash
+
+cd ~/gitclones
+git config --global user.name "root" > /dev/null 2>&1
+git config --global user.email demo@hashicorp.com > /dev/null 2>&1
+
 for x in hashicups-development-team/hashicups-application hashicups-development-team/hashicups-application-modular network-team/terraform-aws-network-module database-team/terraform-aws-postgres-rds-module server-team/terraform-aws-server-module; do
   IFS='/'
   read -a strarr<<< $x
-  cd field-demos-terraform-land-team-Apollo-11-assets/${strarr[1]}
+  cd ~/gitclones/${strarr[1]}
   rm -Rf .git
   git init
+  git config http.postBuffer 524288000 #~502 errors
   git remote add origin http://$GITLAB_PUBLIC_ADDRESS/${strarr[0]}/${strarr[1]}.git
   git add .
   git commit -m "First commit"
@@ -13,6 +20,6 @@ for x in hashicups-development-team/hashicups-application hashicups-development-
   git branch stage
   git push http://root:$GITLAB_PASSWORD@$GITLAB_PUBLIC_ADDRESS/${strarr[0]}/${strarr[1]}.git stage
   git branch development
-  sgit push http://root:$GITLAB_PASSWORD@$GITLAB_PUBLIC_ADDRESS/${strarr[0]}/${strarr[1]}.git development
+  git push http://root:$GITLAB_PASSWORD@$GITLAB_PUBLIC_ADDRESS/${strarr[0]}/${strarr[1]}.git development
   cd ..
 done
